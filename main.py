6faf8ac6cf9c3bb93008dbd2a87843b8ed10b16d
Each Chen <iach526526@gmail.com>
2025-04-27 13:40:11 +0800

    fate: function call

--- main.py
+++ main.py
@@ -2,33 +2,36 @@ import pdfplumber
 import re
 import csv
 
+def no_img_pdf(filename):
+    # 分類對應
+    category={}
+    # 開啟 pdf
+    with pdfplumber.open(f".\\src\\{filename}.pdf") as pdf:
+        extracted_data = []
 
-# 分類對應
-category={}
-# 開啟 pdf
-with pdfplumber.open(".\\src\\car-rule-OX.pdf") as pdf:
-    extracted_data = []
+        for page in pdf.pages:
+            tables = page.extract_tables()
+            for table in tables:
+                for row in table:
+                    if len(row) >= 4:
+                        # row[0] = 題號, row[1] = 正確correct_ans, row[2] = 題目, row[3] = 分類編號
+                        correct_ans = row[1].strip() if row[1] else ''
+                        question = row[2].strip().replace("\n", "") if row[2] else ''
+                        comment = f"法規選擇題第{row[0].strip() if row[1] else ''}題,分類{row[3]}|{category.get(row[3], '')}"
+                        # 只抓有內容的
+                        if correct_ans and question:
+                            extracted_data.append([correct_ans, question,comment])
+                    if len(row)==2:
+                        category[row[0]] = row[1].strip().replace("\n", "") if row[1] else ''
+    # 刪掉表格第一行 中文標示 答案 題目
+    del extracted_data[0]
+    # 存成 CSV
+    with open(f"./gen/{filename}.csv", "w", newline="", encoding="utf-8-sig") as f:
+        writer = csv.writer(f)
+        writer.writerows(extracted_data)
 
-    for page in pdf.pages:
-        tables = page.extract_tables()
-        for table in tables:
-            for row in table:
-                if len(row) >= 4:
-                    # row[0] = 題號, row[1] = 正確correct_ans, row[2] = 題目, row[3] = 分類編號
-                    correct_ans = row[1].strip() if row[1] else ''
-                    question = row[2].strip().replace("\n", "") if row[2] else ''
-                    comment = f"法規選擇題第{row[0].strip() if row[1] else ''}題,{category.get(row[3], '')}"
-                    # 只抓有內容的
-                    if correct_ans and question:
-                        extracted_data.append([correct_ans, question,comment])
-                if len(row)==2:
-                    category[row[0]] = row[1].strip().replace("\n", "") if row[1] else ''
-# 刪掉表格第一行 中文標示 答案 題目
-del extracted_data[0]
-# 存成 CSV
-with open("./gen/car-rule-OX.csv", "w", newline="", encoding="utf-8-sig") as f:
-    writer = csv.writer(f)
-    writer.writerow(["correct_ans", "question"])
-    writer.writerows(extracted_data)
+    print(f"✅ 汽車法規題目 {filename}.pdf 已儲轉換到 {filename}.csv")
 
-print("✅ 汽車法規是非題已儲存到 car-rule-OX.csv")
+if __name__=='__main__':
+    no_img_pdf("car-rule-OX")
+    no_img_pdf("car-rule-mc")
